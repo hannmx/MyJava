6e392caf8f854c0d9b504dccecf2ac637a2bb2e2
hannmx <mxkud@mail.ru>
2023-10-15 19:19:01 +0300

    Changed class NoteProcess and added class FileWriterFun

---
+++ src/main/java/org/example/FileWriterFun.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterFun {
+}
--- src/main/java/org/example/NoteProcess.java
+++ src/main/java/org/example/NoteProcess.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class NoteProcess {
+    public static String processNote(String note) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String formatDate = dateFormat.format(new Date());
+        return formatDate + " -> " + note;
+    }
 }

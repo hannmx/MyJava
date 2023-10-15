56217f36639d42f1b2f3d8da7e55ffc023169025
hannmx <mxkud@mail.ru>
2023-10-15 19:26:50 +0300

    Changed class FileWriterFun

--- src/main/java/org/example/FileWriterFun.java
+++ src/main/java/org/example/FileWriterFun.java
@@ -4,4 +4,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileWriterFun {
+    public static void writeToFile(String content, String filePath){
+        try(FileWriter writer = new FileWriter(filePath, true)) {
+            writer.write(content + "\n");
+            System.out.println("Заметка успешно добавлена в файл.");
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи в файл: " + e.getMessage());
+        }
+    }
 }

a33be2876db403d9b65bbe44d37032a75f65a65c
hannmx <mxkud@mail.ru>
2023-10-15 13:41:38 +0300

    Change class Input

--- src/main/java/org/example/Input.java
+++ src/main/java/org/example/Input.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Input {
+    public static String getUserInput() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите заметку: ");
+        return in.nextLine();
+    }
 }
